9cddaacc0591a88644595ab6126318b799ad0310
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-19 15:00:12 -0700

    add a splash page that displays all engineers and machines

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
   public class HomeController: Controller
   {
-    [HttpGet("/")]
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+    
     public ActionResult Index()
     {
       return View();
     }
     
+    public ActionResult Splash()
+    {
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
+      return View();
+    }
   }
 }
