4a55491c6ba0f8c9961f0325ef9c6af757becf37
Woo Jin Kim <kimwoojin211@gmail.com>
2021-03-19 08:47:43 -0700

    complete setup of all model files and build database.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Machine
   {
     public Machine()
     {
-
+      this.JoinEntities = new HashSet<EngineerMachine>();
     }
+
+    public int MachineId { get;set; } 
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public string Condition { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+    
   }
 }
